a4fbd42873e856a47a8dfc8a23d4ef3518c137dc
phamchinang1510 <phamchinang.dev@gmail.com>
2022-01-30 18:49:40 +0700

    update

---
+++ skygarden/skygarden/flower.h
@@ -0,0 +1,10 @@
+﻿#ifndef FLOWER_H
+#define FLOWER_H
+class flower
+{
+public:	
+	int click; // Số lần click
+	int flowerType; // Loại hoa
+	int stage; // Giai đoạn
+};
+#endif
---
+++ skygarden/skygarden/pot.h
@@ -0,0 +1,11 @@
+﻿#ifndef POT_H
+#define POT_H
+class pot
+{
+public:	
+	int x,y,b,t; // Trái x, Phải y, Dưới b(bottom), Trên t(top)
+	int click; // Số lần click
+	int potType; // Loại chậu
+	bool planted; // Ktra đã trồng
+};
+#endif
